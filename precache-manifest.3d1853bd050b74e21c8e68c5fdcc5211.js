5edf1ed7d06a19dc940a97c5e17f839ba3ef5a17
ranarupesh <rupeshrana288@gamil.com>
2020-08-08 12:00:22 +0530

    Updates

---
+++ precache-manifest.3d1853bd050b74e21c8e68c5fdcc5211.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97d73c04b7b95845d3c7414083a832e",
+    "url": "/React_youtube_clone/index.html"
+  },
+  {
+    "revision": "d1867ae006f8e4c876b5",
+    "url": "/React_youtube_clone/static/css/main.3d8a9da0.chunk.css"
+  },
+  {
+    "revision": "06d6e154f7284a395de4",
+    "url": "/React_youtube_clone/static/js/2.ac0f785f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React_youtube_clone/static/js/2.ac0f785f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1867ae006f8e4c876b5",
+    "url": "/React_youtube_clone/static/js/main.32906397.chunk.js"
+  },
+  {
+    "revision": "158df9eaab425b918a95",
+    "url": "/React_youtube_clone/static/js/runtime-main.1fcee687.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React_youtube_clone/precache-manifest.3d1853bd050b74e21c8e68c5fdcc5211.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_youtube_clone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
